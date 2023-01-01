0cfeabf72040ccdfa4ee0dee121d117ec7882f51
wvdveer <ward@vanderveer.id>
2023-01-02 07:47:11 +1000

    custom names

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ The macro "json_schema_here" allows the inclusion of JSON Schema directly within
 while the macro "json_schema_file" specified that the JSON Schema document is in a file at the location specified.
 
 It is required that the JSON Schema have a "title" specified, this value will be used as the Rust name of the struct.
+However, if json_schema_file is used, a mssing title can be supplied by using a custom name instead of blank, e.g.
+"->My_Struct_Name" will name the struct My_Struct_Name where "title" is missing.
 
 JSON objects that exist under the main object (e.g. where there is an array of objects), should be specified in the 
 "$defs" object.  These should be referenced as { "$ref": "#/$defs/*name*"} in the main object.  Each object under $defs 
@@ -37,11 +39,22 @@ json_schema_file!("src/example.json");
 will read the JSON Schema from the file src/example.json.  This path is relative to the location of the Cargo.toml file.
 
 
-It is possible to also specify custom maps to override the Rust type given to fields, 
+It is possible to also specify custom maps to override the names and types that would otherwise be used.
+
+e.g.
+```
+json_schema_file!("src/example.json", "bank statement->Bank_Statement");
+```
+
+will change the field or struct named "bank statement" to use the name "BankStatement" in Rust.
+
+To overide the Rust type given to fields, use "*field name*=*Rust type*"
 
 e.g.
 ```
 json_schema_file!("src/example.json", "amount=i64");
 ```
 
-will override the field named "amount" to be an i64 instead of whatever type would otherwise be assigned.
+will override the field named "amount" to be an i64 instead of whatever type would otherwise be assigned.  The field name
+specified be the original name without the any custom name override.
+
--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,9 @@ use std::collections::HashMap;
 #[proc_macro]
 pub fn json_schema_here(schema_body: TokenStream) -> TokenStream {
     let schema_text: String = schema_body.to_string(); 
+    let custom_name_map: HashMap<String, String> = HashMap::new();
     let custom_type_map: HashMap<String, String> = HashMap::new();
-    let struct_text = json_schema_to_struct(&schema_text, &custom_type_map);
+    let struct_text = json_schema_to_struct(&schema_text, &custom_name_map, &custom_type_map);
     let ts  = TokenStream::from_str(&struct_text.unwrap());
     match ts {
         Ok(rslt)      => return rslt,
@@ -24,18 +25,21 @@ pub fn json_schema_here(schema_body: TokenStream) -> TokenStream {
 }
 
 /// include JSON Schema from a file
-/// supports custom types 
+/// supports custom names and types 
 /// 
-/// json_schema_file("<filename>", "<custom_type1>", "<custom_type2>", etc);
+/// json_schema_file("<filename>", "<custom_type1>", "<custom_name1>", "<custom_name2>", "<custom_type2>", ...);
 /// 
 /// <filename> is relative path, e.g. "src/schema.json"
 /// 
 /// <custom_type> is "name=type", e.g. "flag=bool"
 /// 
+/// <custom_name is "old_name->new_name", e.g. "my field->my_field"
+/// 
 #[proc_macro]
 pub fn json_schema_file(parameters: TokenStream) -> TokenStream {
     let mut parameter_number: i32 = 1;
     let mut file_path: String = "".to_string();
+    let mut custom_name_map: HashMap<String, String> = HashMap::new();
     let mut custom_type_map: HashMap<String, String> = HashMap::new();
     for parameter in parameters.into_iter() {
         let syntax: String = parameter.to_string();
@@ -45,16 +49,24 @@ pub fn json_schema_file(parameters: TokenStream) -> TokenStream {
         let param: String = syntax[1..syntax.len()-1].to_string();
         if parameter_number == 1 {
             file_path = param;
-        } else {
+        } else if param.contains("=") {
+            // type override
             let custom_type_parts: Vec<&str> = param.split("=").collect();
             if custom_type_parts.len() != 2 {
-                panic!("Could not parse JSON Schema Invalid overrides: {}\n", param);
+                panic!("Could not parse JSON Schema Invalid custom type: {}\n", param);
             }
             custom_type_map.insert(custom_type_parts[0].to_string(), custom_type_parts[1].to_string());
-        }
+        } else if param.contains("->") {
+            // name override
+            let custom_name_parts: Vec<&str> = param.split("->").collect();
+            if custom_name_parts.len() != 2 {
+                panic!("Could not parse JSON Schema Invalid custom name: {}\n", param);
+            }
+            custom_name_map.insert(custom_name_parts[0].to_string(), custom_name_parts[1].to_string());
+        }    
         parameter_number = parameter_number + 1;
     }
-    let struct_text = json_schema_file_impl(file_path.clone(), &custom_type_map);
+    let struct_text = json_schema_file_impl(file_path.clone(), &custom_name_map, &custom_type_map);
     let ts  = TokenStream::from_str(&struct_text);
     match ts {
         Ok(rslt)      => return rslt,
@@ -63,10 +75,10 @@ pub fn json_schema_file(parameters: TokenStream) -> TokenStream {
 }    
 
 /// implementation of json_schema_file macro code
-fn json_schema_file_impl(file_path: String, custom_type_map: &HashMap<String, String>) -> String {
+fn json_schema_file_impl(file_path: String, custom_name_map: &HashMap<String, String>, custom_type_map: &HashMap<String, String>) -> String {
     let schema_text: String = fs::read_to_string(&file_path)
         .expect(format!("Could not read JSON Schema file: {}\n", &file_path).as_str());
-    let struct_text = json_schema_to_struct(&schema_text, custom_type_map);
+    let struct_text = json_schema_to_struct(&schema_text, custom_name_map, custom_type_map);
     match struct_text {
         Ok(rslt)      => return rslt,
         Err(err_msg)   => panic!("Could not parse error {} from JSON Schema {}\n", err_msg, schema_text)
@@ -74,7 +86,7 @@ fn json_schema_file_impl(file_path: String, custom_type_map: &HashMap<String, St
 }
 
 /// convert JSON Schema in a string slice to a Rust struct
-fn json_schema_to_struct(schema_text: &str, custom_type_map: &HashMap<String, String>) -> Result<String> {
+fn json_schema_to_struct(schema_text: &str, custom_name_map: &HashMap<String, String>, custom_type_map: &HashMap<String, String>) -> Result<String> {
     let schema_json_maybe: Result<Value> = serde_json::from_str(schema_text);
     let schema_json_value: Value = match schema_json_maybe {
         Ok(sj)        => sj,
@@ -84,37 +96,42 @@ fn json_schema_to_struct(schema_text: &str, custom_type_map: &HashMap<String, St
         Value::Object(obj)  => obj,
         _                   => panic!("Could not parse JSON Schema not JSON Object\n")
     };
-    json_schema_map_to_struct(&schema_json_map, custom_type_map)
+    json_schema_map_to_struct(&schema_json_map, custom_name_map, custom_type_map)
 }
 
 /// convert JSON Schema in a serde JSON map to a Rust struct
-fn json_schema_map_to_struct(schema_json_map: &Map<String, Value>, custom_type_map: &HashMap<String, String>) -> Result<String> {
-    if schema_json_map.contains_key("title") == false || schema_json_map["title"].as_str() == None {
-        panic!("Could not parse JSON Schema, no title\n");
-    }
+fn json_schema_map_to_struct(schema_json_map: &Map<String, Value>, custom_name_map: &HashMap<String, String>, custom_type_map: &HashMap<String, String>) -> Result<String> {
+    let title: String = if schema_json_map.contains_key("title") == false || schema_json_map["title"].as_str() == None {
+        if custom_name_map.contains_key("") {
+            custom_name_map.get("").unwrap().to_string()
+        } else {
+            panic!("Could not parse JSON Schema, no title\n");
+        }
+    } else {
+        format_struct_name(schema_json_map["title"].as_str().unwrap(), custom_name_map)
+    };
     if schema_json_map.contains_key("properties") == false {
         panic!("Could not parse JSON Schema, no properties\n");
     }
-    let title: String = capitalise(schema_json_map["title"].as_str().unwrap());
     let mut rslt: String = format!("#[derive(Clone, Serialize, Deserialize)]\r\npub struct {} {{\n", title);
     let props_value: Value = schema_json_map["properties"].clone();
     if let Value::Object(props_map) = props_value {
         for props_map_item in props_map.iter() {
             let key_name = props_map_item.0.clone();
             let defn_value = props_map_item.1.clone();
-            let field_text: String = get_field_text(&key_name, &defn_value, custom_type_map);
+            let field_text: String = get_field_text(&key_name, &defn_value, custom_name_map, custom_type_map);
             rslt = format!("{}{}", rslt, field_text);
         }
     }
     rslt = rslt + "}\n";
     if schema_json_map.contains_key("$defs") {
-        rslt = format!("{}{}", rslt, process_defs(&schema_json_map["$defs"], custom_type_map));
+        rslt = format!("{}{}", rslt, process_defs(&schema_json_map["$defs"], custom_name_map, custom_type_map));
     }
     return Ok(rslt);
 }
 
 /// process the &defs field
-fn process_defs(defs_value: &Value, custom_type_map: &HashMap<String, String>) -> String {
+fn process_defs(defs_value: &Value, custom_name_map: &HashMap<String, String>, custom_type_map: &HashMap<String, String>) -> String {
     let mut rslt: String = "".to_string();
     if let Value::Object(defs_map) = defs_value {
         for defs_map_item in defs_map.iter() {
@@ -123,7 +140,7 @@ fn process_defs(defs_value: &Value, custom_type_map: &HashMap<String, String>) -
             if let Value::Object(defn_map) = &defn_value {
                 let mut defn_map_mut = defn_map.clone();
                 defn_map_mut.insert("title".to_string(), Value::String(key_name.to_string()));
-                let this_def: Result<String> = json_schema_map_to_struct(&defn_map_mut, custom_type_map);
+                let this_def: Result<String> = json_schema_map_to_struct(&defn_map_mut, custom_name_map, custom_type_map);
                 if let Ok(this_ok) = this_def {
                     rslt = format!("{}\n\n{}", rslt, this_ok);
                 } else {
@@ -140,10 +157,14 @@ fn process_defs(defs_value: &Value, custom_type_map: &HashMap<String, String>) -
 }
 
 /// convert a property to a Rust field declaration
-fn get_field_text(key_name: &str, defn_value: &Value, custom_type_map: &HashMap<String, String>) -> String {
+fn get_field_text(key_name: &str, defn_value: &Value, custom_name_map: &HashMap<String, String>, custom_type_map: &HashMap<String, String>) -> String {
+    let mut field_name: String = key_name.to_string();
+    if custom_name_map.contains_key(key_name) {
+        field_name = custom_name_map.get(key_name).unwrap().to_string();
+    }
     if custom_type_map.contains_key(key_name) {
         let rust_type_name: String = custom_type_map.get(key_name).unwrap().to_string();
-        return format!("    #[serde(default)]\n    pub {}: {},\n", key_name, rust_type_name);
+        return format!("    #[serde(default)]\n    pub {}: {},\n", field_name, rust_type_name);
     }
     if let Value::Object(defn_map) = defn_value {
         if defn_map.contains_key("type") == false {
@@ -168,7 +189,7 @@ fn get_field_text(key_name: &str, defn_value: &Value, custom_type_map: &HashMap<
                                        // $ref
                                        if let Value::String(ref_name) = &item_type_map["$ref"] {
                                            if &ref_name[0..8] == "#/$defs/" {
-                                               let referenced_type = capitalise(&ref_name[8..]);
+                                               let referenced_type = format_struct_name(&ref_name[8..], custom_name_map);
                                                referenced_type
                                            } else {
                                                panic!("Could not parse JSON Schema, unknown type {}\n", json_type_name);
@@ -188,7 +209,7 @@ fn get_field_text(key_name: &str, defn_value: &Value, custom_type_map: &HashMap<
                                 full_name
                             }     
         };
-        return format!("    #[serde(default)]\n    pub {}: {},\n", key_name, rust_type_name);
+        return format!("    #[serde(default)]\n    pub {}: {},\n", field_name, rust_type_name);
     } else {
         panic!("Could not parse JSON Schema, bad defintion for {}\n", key_name);
     }   
@@ -207,8 +228,11 @@ fn get_simple_rust_type(json_type_name: &str) -> String {
     return rust_type_name.to_string();
 }
 
-/// convert string to Capital Case
-fn capitalise(src: &str) -> String {
+/// apply name changes, or else convert string to Capital Case 
+fn format_struct_name(src: &str, custom_name_map: &HashMap<String, String>) -> String {
+    if custom_name_map.contains_key(src) {
+        return custom_name_map.get(src).unwrap().to_string();
+    } 
     let lc: String = src.to_string();
     return lc[0..1].to_uppercase() + &lc[1..];
 }
@@ -224,8 +248,9 @@ mod tests {
         let contents: String = fs::read_to_string(file_path)
         .expect("Could not read example file\n");
 
+        let custom_name_map: HashMap<String, String> = HashMap::new();
         let custom_type_map: HashMap<String, String> = HashMap::new();
-        let ts = json_schema_to_struct(&contents, &custom_type_map);
+        let ts = json_schema_to_struct(&contents, &custom_name_map, &custom_type_map);
         print!("{}\r\n", ts.unwrap());
     }
 
@@ -235,17 +260,20 @@ mod tests {
         let contents: String = fs::read_to_string(file_path)
             .expect("Could not read example file 2\n");
 
+        let custom_name_map: HashMap<String, String> = HashMap::new();
         let custom_type_map: HashMap<String, String> = HashMap::new();
-        let ts = json_schema_to_struct(&contents, &custom_type_map);
+        let ts = json_schema_to_struct(&contents, &custom_name_map, &custom_type_map);
         print!("{}\r\n", ts.unwrap());
     }
 
     #[test]
     fn process_example_file2_as_file() {
         let file_path: String = "src/example2.json".to_string();
+        let mut custom_name_map: HashMap<String, String> = HashMap::new();
+        custom_name_map.insert("veggie".to_string(), "Vegetable".to_string());
         let mut custom_type_map: HashMap<String, String> = HashMap::new();
         custom_type_map.insert("veggieLike".to_string(), "i32".to_string());
-        let ts: String = json_schema_file_impl(file_path, &custom_type_map);
+        let ts: String = json_schema_file_impl(file_path, &custom_name_map, &custom_type_map);
 
         print!("{}\r\n", ts);
     }
